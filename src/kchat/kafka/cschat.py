67fb17ec2890256d9d8371351894d331b78ba462
hahahellooo <hahahello777@gmail.com>
2024-08-21 17:22:53 +0900

    consumer chat

---
+++ src/kchat/kafka/cschat.py
@@ -0,0 +1,27 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+        'chat',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        group_id="chat-group",
+        enable_auto_commit=True,
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+)
+
+print("채팅 프로그램 - 메시지 수신")
+print("메시지 대기 중 ...")
+
+# 예외 처리하는 구문
+try:
+    for m in consumer:
+        data = m.value
+        print(f"[FRIEND] {data['message']}")
+
+except KeyboardInterrupt:
+    print("채팅 종료")
+
+finally:
+    consumer.close()
+
