0aad471660f6e079c709ab7d92a41fe35f5e7ff6
hahahellooo <hahahello777@gmail.com>
2024-08-22 17:34:27 +0900

    connect with docker:8484

--- src/kchat/kafka/pd.py
+++ src/kchat/kafka/pd.py
@@ -4,14 +4,18 @@ import json
 from tqdm import tqdm 
 
 pd = KafkaProducer(
-        bootstrap_servers=['localhost:9092'], value_serializer=lambda x:json.dumps(x).encode('utf-8')
+        bootstrap_servers=['172.17.0.1:9092'],
+        value_serializer=lambda x:json.dumps(x).encode('utf-8'),
+        compression_type='gzip',
+        batch_size=100
+        
         )
 start = time.time()
 
-for i in tqdm(range(10)):
+for i in tqdm(range(10000)):
     data = {'str':'value' + str(i)}
-    pd.send('topic1', value=data)
+    pd.send('test-gzip-4', value=data)
     pd.flush()
-    time.sleep(1)
+    time.sleep(0.001)
 end = time.time()
 print("[DONE]:", end - start)
